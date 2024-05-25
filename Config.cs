dffb1a27b288425c296f5f2b899705217be41cf0
yejian <596306006@qq.com>
2024-05-25 12:31:57 +0800

    fix:change opengl version 4.1 and refresh token

--- Config.cs
+++ Config.cs
@@ -1,11 +1,16 @@
 public class Config
 {
+#if LOCALDEV
     public static string APIAddr = "http://192.168.101.9:10002";
     public static string WsAddr = "ws://192.168.101.9:10001";
-    public static int PlatformID = 3; //windows
+#else
+    public static string APIAddr = "http://14.29.168.56:10002";
+    public static string WsAddr = "ws://14.29.168.56:10001";
+#endif
     public static string DataDir = "./";
     public static uint LogLevel = 5;
     public static bool IsLogStandardOutput = true;
     public static string LogFilePath = "./log/";
     public static bool IsExternalExtensions = true;
+
 }
